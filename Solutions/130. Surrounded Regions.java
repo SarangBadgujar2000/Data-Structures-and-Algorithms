e03e777ad58a784ff29426e29ed12233bf979f07
Sarang <sarangrajendrabadgujar@gmail.com>
2023-10-25 18:36:59 +0530

    130. Surrounded Regions

---
+++ Solutions/130. Surrounded Regions.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void dfs(char[][] board,int i,int j){
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!='O'){
+            return;
+        }
+       
+        board[i][j]='*';
+
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
+        return;
+    }
+    public void solve(char[][] board) {
+        if(board.length<=1 || board[0].length<=1) return;
+        int m=board.length;
+        int n=board[0].length;
+
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
+            }
+            if(board[i][n-1]=='O'){
+                dfs(board,i,n-1);
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(board[0][j]=='O'){
+                dfs(board,0,j);
+            }
+            if(board[m-1][j]=='O'){
+                dfs(board,m-1,j);
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='*'){
+                    board[i][j]='O';
+                }else{
+                    board[i][j]='X';
+                }
+            }
+        }
+        
+    }
+}
